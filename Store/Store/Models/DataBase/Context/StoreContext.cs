88b0bd140312c69bebccb4a82b750f218339f4d5
afshin m <afshinmoradii@hotmail.com>
2019-06-21 20:33:54 +0430

    تنظیمات اولیه پروژه

---
+++ Store/Store/Models/DataBase/Context/StoreContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Models.DataBase.Context
+{
+    public class StoreContext:DbContext
+    {
+    }
+}
--- Store/Store/Startup.cs
+++ Store/Store/Startup.cs
@@ -2,19 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Store.Models.DataBase.Context;
 
 namespace Store
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<StoreContext>(option =>
+            option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //Configure Authentication By Cooki Base
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+
+            }).AddCookie(option =>
+            {
+                option.AccessDeniedPath = "/AccessDenied";
+                option.LoginPath = "/Login";
+                option.LogoutPath = "/Logout";
+                option.ExpireTimeSpan = TimeSpan.FromDays(7);
+
+            });
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,7 +54,15 @@ namespace Store
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
