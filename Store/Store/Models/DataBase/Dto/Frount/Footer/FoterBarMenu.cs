9297ed489b8d7f9dda64c2475b4eb18113a841c5
moradisaed5 <moradisaed5@gmail.com>
2019-07-22 06:19:43 +0430

    Update SiteFooterService

--- Store/Store/Models/DataBase/Dto/Frount/Footer/FoterBarMenu.cs
+++ Store/Store/Models/DataBase/Dto/Frount/Footer/FoterBarMenu.cs
@@ -7,6 +7,10 @@ namespace Store.Models.DataBase.Dto.Frount.Footer
 {
     public class FoterBarMenu
     {
-
+        public string ContactUsDescription { get; set; }
+        public string ContactUsAddress { get; set; }
+        public string ContactUsEmail { get; set; }
+        public string ContactUsTel { get; set; }
+        public string ContactusWorkTime { get; set; }
     }
 }
--- Store/Store/Models/Services/Frount/SiteFooterService.cs
+++ Store/Store/Models/Services/Frount/SiteFooterService.cs
@@ -1,4 +1,5 @@
-﻿using Store.Models.DataBase.Dto.Frount.Footer;
+﻿using Newtonsoft.Json;
+using Store.Models.DataBase.Dto.Frount.Footer;
 using Store.Models.IService;
 using System;
 using System.Collections.Generic;
@@ -7,16 +8,55 @@ using System.Threading.Tasks;
 
 namespace Store.Models.Services.Frount
 {
+    /// <summary>
+    /// سرویس فوتر
+    /// </summary>
     public class SiteFooterService : ISiteFooterService
     {
+        /// <summary>
+        ///  عضویت در خبرنامه
+        /// </summary>
+        /// <param name="email">ایمیل</param>
+        /// <returns></returns>
         public FoterBarMenu GetFooterMenu()
         {
-            throw new NotImplementedException();
+            using (Store.Models.DataBase.Context.StoreContext _Storedb = new DataBase.Context.StoreContext(null))
+            {
+                return JsonConvert.DeserializeObject<FoterBarMenu>(_Storedb.Settings.First().ContactUs_Json);
+            }
         }
-
+        /// <summary>
+        ///دریافت منو فوتر
+        /// </summary>
+        /// <returns></returns>
         public bool SubscribeByEmail(string email)
         {
-            throw new NotImplementedException();
+            using (Store.Models.DataBase.Context.StoreContext _Storedb = new DataBase.Context.StoreContext(null))
+            {
+                try
+                {
+                    var result = _Storedb.NewsLetters.Where(x => x.Email == email);
+                    if (result.Count() > 0)
+                    {
+                        result.First().IsDeleted = false;
+                        result.First().Token = string.Empty.GetNewToken();
+                    }
+                    else
+                    {
+                        _Storedb.NewsLetters.Add(new DataBase.Entities.NewsLetter() {
+                            Email = email,
+                            IsDeleted = false,
+                            Token = string.Empty.GetNewToken()
+                        });
+                    }
+                    _Storedb.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
         }
     }
 }
